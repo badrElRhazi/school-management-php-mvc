f0aff3479aa3ae245bfa577b607cc668f1e18133
Badr El Rhazi <rzi.badr@gmail.com>
2023-06-08 15:38:43 +0100

    add Update student infos button

--- Views/liste_stagiaires.php
+++ Views/liste_stagiaires.php
@@ -26,6 +26,7 @@ ob_start();
                 <td><?=$stagiaire->age?></td>
                 <td><?=$stagiaire->login?></td>
                 <td>
+                <a href="edit.php?id=<?=$stagiaire->id?> " class="btn btn-primary btn-sm">Modifier</a>
                     <a href="delete.php?id=<?php echo $stagiaire->id?> " class="btn btn-danger btn-sm">Supprimer</a>
                 </td>
             </tr>
