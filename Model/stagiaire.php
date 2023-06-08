92490f197e9c9029bb6a478a7dcaf9563543ac73
Badr El Rhazi <rzi.badr@gmail.com>
2023-06-08 15:41:30 +0100

    update,delete,select functions SQL queries

--- Model/stagiaire.php
+++ Model/stagiaire.php
@@ -20,11 +20,37 @@ function create(){
     $sqlState = $pdo->prepare('INSERT INTO stagiaire VALUES(null,?,?,?,?,?)');
     return $sqlState->execute([$nom, $prenom, $age, $login, $password]); 
 }
-function update(){
+function edit($id, $nom, $prenom, $age, $login, $password){
+
+    $pdo=database_connection();
+    $sqlState = $pdo->prepare("UPDATE stagiaire 
+                SET nom = ?, 
+                    prenom = ?,
+                    age = ?,
+                    login = ?,
+                    password= ?
+                WHERE id=?
+    ");
     
-}
-function destroy(){
+    return $sqlState->execute([$nom, $prenom, $age, $login, $password, $id]);
 
 }
+function destroy($id){
+    $pdo=database_connection();
+    $nom=$_POST['nom'];
+    $prenom=$_POST['prenom'];
+    $age=$_POST['age'];
+    $login=$_POST['login'];
+    $password=$_POST['password'];
+    $sqlState = $pdo->prepare("DELETE FROM stagiaire WHERE id=? ");
+    return $sqlState->execute([$nom, $prenom, $age, $login, $password]); 
 
+
+}
+function view($id){
+    $pdo=database_connection();
+    $sqlState = $pdo->prepare("SELECT * FROM stagiaire WHERE id= ? ");
+    $sqlState->execute([$id]);
+    return $sqlState->fetch(PDO::FETCH_OBJ);
+}
 ?>
